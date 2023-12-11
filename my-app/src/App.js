8f690d48b055d2a44201c2fdd0891694f26fef42
syedmuttalib <muttalibahmed28@gmail.com>
2023-12-11 19:16:25 +0530

    video 5 comlpeted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-let name ="SYed "
+
 function App() {
   return (
 <>
-<nav>
-  <li>Home</li>
-  <li>About</li>
-  <li>Service</li>
-  <li>Login</li>
-</nav>
-  <div className="blank">
-    <li className="name">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius enim aliquam magni numquam reiciendis ad debitis voluptatibus dicta. In sapiente quaerat unde, et sequi corporis nobis saepe natus omnis!
-    </li>
-    <h1>Hellow {name}</h1>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TexUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
+</nav>
 </>
   );
 }
