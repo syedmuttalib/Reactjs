6f6cf28f96025562199d97f239c76985a1c655ef
syedmuttalib <muttalibahmed28@gmail.com>
2023-12-11 20:34:49 +0530

    added props example

---
+++ propsexm/module1.mjs
@@ -0,0 +1,10 @@
+const a="syed"
+const b="Muttalib"
+const c="Ahmed"
+const d="Hyderabad"
+
+export default a
+// jo bhi variable by default export nahi hori to aae sa lekhna 
+export{b}
+export{c}
+export{d}
---
+++ propsexm/module2.mjs
@@ -0,0 +1,3 @@
+import dza, {b,c,d} from './module1.mjs'
+console.log(dza)
+console.log(b,c,d)
