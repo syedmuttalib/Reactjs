5be15086e03d39b976229f1b4f6d88cb2c573093
syedmuttalib <muttalibahmed28@gmail.com>
2023-12-11 15:42:50 +0530

    added new feature

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name ="SYed "
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<nav>
+  <li>Home</li>
+  <li>About</li>
+  <li>Service</li>
+  <li>Login</li>
+</nav>
+  <div className="blank">
+    <li className="name">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque eius enim aliquam magni numquam reiciendis ad debitis voluptatibus dicta. In sapiente quaerat unde, et sequi corporis nobis saepe natus omnis!
+    </li>
+    <h1>Hellow {name}</h1>
+  </div>
+</>
   );
 }
 
