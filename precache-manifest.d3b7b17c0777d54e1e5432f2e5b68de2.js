4711ccd4863339eac758d968c28b43f76a20804a
syedmuttalib <muttalibahmed28@gmail.com>
2023-12-20 12:59:22 +0530

    Updates

---
+++ precache-manifest.d3b7b17c0777d54e1e5432f2e5b68de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae85af1314d2e10bc15d4e2f75abfbd7",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8becd7e94c58f098ae9e",
+    "url": "/my-app/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/my-app/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/my-app/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "8becd7e94c58f098ae9e",
+    "url": "/my-app/static/js/main.e710be53.chunk.js"
+  },
+  {
+    "revision": "963c8cf830fccb149c29",
+    "url": "/my-app/static/js/runtime~main.e92001b1.js"
+  }
+]);
